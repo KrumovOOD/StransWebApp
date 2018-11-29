51efed07d5b62038924d55fa516a40c4ce679ad8
KrumovOOD <krumov_ood@abv.bg>
2018-11-30 01:01:07 +0200

    Add project files.

---
+++ Strans/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using Strans.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Strans.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        STransEntities db = new STransEntities();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(Credential model)
+        {
+            db.Credentials.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successfull", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
